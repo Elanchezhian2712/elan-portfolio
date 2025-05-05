6ab7259ae9f3ec149e5c810cc3bf7aaa79b04f48
Elanchezhian_M <122656808+Elanchezhian2712@users.noreply.github.com>
2025-05-05 14:58:37 +0530

    Update RecentProjects.tsx

--- components/RecentProjects.tsx
+++ components/RecentProjects.tsx
@@ -10,7 +10,7 @@ const RecentProjects = () => {
     <section id="projects" className="py-20">
       <h1 className="heading">
         A small selection of{" "}
-        <span className="text-purple">recent projects</span>
+        <span className="text-purple">Recent Projects</span>
       </h1>
       <div className="flex flex-wrap items-center justify-center p-4 gap-16 mt-10">
         {projects.map((item) => (
@@ -22,9 +22,9 @@ const RecentProjects = () => {
               <div className="relative flex items-center justify-center sm:w-96 w-[80vw] overflow-hidden h-[20vh] lg:h-[30vh] mb-10">
                 <div
                   className="relative w-full h-full overflow-hidden lg:rounded-3xl"
-                  style={{ backgroundColor: "#13162D" }}
+                  
                 >
-                  <img src="/bg.png" alt="bgimg" />
+                 
                 </div>
                 <img
                   src={item.img}
